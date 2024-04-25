dfc67bce9cfc31832d49bce33a4e7c3cc5c8097d
fultlin <ugolok.kita@gmail.com>
2024-04-25 05:02:42 +0300

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/header/Header'
 import Main from './components/main/Main'
-import './App.scss'
+import './styles/App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
