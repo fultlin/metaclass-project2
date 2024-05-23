3548c8e00d1a8ea130803291fff4e8ed2a8be1cb
fultlin <ugolok.kita@hmail.com>
2024-05-23 17:27:05 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import './App.module.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <main>
         <Main />
       </main>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
